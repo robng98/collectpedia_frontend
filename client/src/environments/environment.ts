7cddacd640e6d2f1358d971648fec1787c7d51a5
Roberto Nicácio Guimarães <betonicacio10@gmail.com>
2025-03-19 12:13:25 -0300

    Fix API URL formatting in environment configuration

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://tccapi-production-710e.up.railway.app/api'
+    apiUrl: 'https://tccapi-production-710e.up.railway.app/api/'
 };

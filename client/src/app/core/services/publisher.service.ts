157f1bc24ee27e3884bb55e69d099ffca984f402
Roberto Nicácio Guimarães <betonicacio10@gmail.com>
2025-03-21 00:25:44 -0300

    Add methods to PublisherService for fetching publishers and their series by name and ID

--- client/src/app/core/services/publisher.service.ts
+++ client/src/app/core/services/publisher.service.ts
@@ -1,7 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Publisher } from '../../shared/models/publisher';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Pagination } from '../../shared/models/pagination';
+import { Serie } from '../../shared/models/serie';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +17,17 @@ export class PublisherService {
     return this.http.get<Publisher>(this.baseUrl + 'editora/' + publisherId);
   }
   
+  getPublishers(): Observable<Pagination<Publisher>> {
+    return this.http.get<Pagination<Publisher>>(this.baseUrl + 'editora');
+  }
+
+  getPublishersByName(name: string): Observable<Pagination<Publisher>> {
+    const params = new HttpParams().set('name', name);
+    return this.http.get<Pagination<Publisher>>(this.baseUrl + 'editora/search', { params });
+  }
+
+  getPublisherSeries(publisherId: number): Observable<Pagination<Serie>> {
+    const params = new HttpParams().set('editoraId', publisherId)
+    return this.http.get<Pagination<Serie>>(this.baseUrl + 'serie', {params});
+  }
 }
